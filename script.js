d05fd744fff206e6d96c6d93d9649e29cec9b7d4
MSI\j2026 <test@com>
2026-07-10 21:35:59 +0800

    test 1.0.0.1

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 function updateClock() {
+
     const now = new Date();
 
-    const time = now.toLocaleTimeString();
+    // 时间
+    const time = now.toLocaleTimeString('zh-CN');
 
-    document.getElementById("clock").innerHTML = time;
+    // 日期
+    const date = now.toLocaleDateString('zh-CN');
+
+    // 星期
+    const weeks = [
+        "星期日",
+        "星期一",
+        "星期二",
+        "星期三",
+        "星期四",
+        "星期五",
+        "星期六"
+    ];
+
+    const week = weeks[now.getDay()];
+
+    // 时区
+    const timezone =
+        Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+    document.getElementById("clock").innerHTML =
+        `
+        <h2>${time}</h2>
+        <p>${date}</p>
+        <p>${week}</p>
+        <p>时区：${timezone}</p>
+        `;
 }
 
 updateClock();
